bf15dffee1e27a2eb25353516cd0bb8d014863a5
Camilo Castrillon <cam.castrillon23@outlook.com>
2025-02-06 23:44:57 -0500

    fixed forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,8 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
-        super(CustomUserCreationForm, self).__init__
-            (*args, **kwargs)
+        super(CustomUserCreationForm, self).__init__ (*args, **kwargs)
         for fieldname in ['username', 'password1',
         'password2']:
             self.fields[fieldname].help_text = None
